604a202ea85a477d9780063d7c1856d13f2251cb
George Bukhtaty <gbukhtatyy@gmail.com>
2020-05-05 22:18:29 +0300

    module 6 - Databinding - data binding part 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 
 module.exports = {
   entry: `./src/main.js`,
@@ -10,5 +11,16 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, `public`),
     watchContentBase:true
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MomentLocalesPlugin()
+  ]
 };
